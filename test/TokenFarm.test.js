15503f897ec08206100be2b42f871f2c04ab9be9
Simbamon <sbhan9511@gmail.com>
2021-11-10 19:58:27 +0900

    cont

--- test/TokenFarm.test.js
+++ test/TokenFarm.test.js
@@ -80,6 +80,20 @@ contract('TokenFarm', ([owner, investor]) => {
             assert.equal(result.toString(), tokens('100'), 'investor DApp Token wallet balance correct after issuance')
 
             await tokenFarm.issueTokens({ from: investor}).should.be.rejected;
+
+            await tokenFarm.unstakeTokens({ from: investor })
+
+            result = await daiToken.balanceOf(investor)
+            assert.equal(result.toString(), tokens('100'), 'investor Mock DAI wallet balance correct after staking')
+
+            result = await daiToken.balanceOf(tokenFarm.address)
+            assert.equal(result.toString(), tokens('0'), 'Token Farm Mock DAI balance correct after staking')
+
+            result = await tokenFarm.stakingBalance(investor)
+            assert.equal(result.toString(), tokens('0'), 'investor staking balance correct after staking')
+
+            result = await tokenFarm.isStaking(investor)
+            assert.equal(result.toString(), 'false', 'investor staking status correct after staking')
         })
     })
 
