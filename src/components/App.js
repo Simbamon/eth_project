f48fd41b894c9325a04a5fc2bd942cc9048b7950
Simbamon <sbhan9511@gmail.com>
2021-12-19 22:41:58 +0900

    load crypto data

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 import Web3 from 'web3'
+import DaiToken from '../abis/DaiToken.json'
+import DappToken from '../abis/DappToken.json'
+import TokenFarm from '../abis/TokenFarm.json'
 import Navbar from './Navbar'
 import './App.css'
 
@@ -15,6 +18,42 @@ class App extends Component {
     
     const accounts = await web3.eth.getAccounts()
     this.setState({ account: accounts[0]})
+
+    const networkId = await web3.eth.net.getId()
+
+    const daiTokenData = DaiToken.networks[networkId]
+    if(daiTokenData) {
+      const daiToken = new web3.eth.Contract(DaiToken.abi, daiTokenData.address)
+      this.setState({ daiToken })
+      let daiTokenBalance = await daiToken.methods.balanceOf(this.state.account).call()
+      this.setState({ daiTokenBalance: daiTokenBalance.toString() })
+    }
+    else {
+      window.alert('DaiToken contract not deployed to detected network.')
+    }
+
+    const dappTokenData = DappToken.networks[networkId]
+    if(dappTokenData) {
+      const dappToken = new web3.eth.Contract(DappToken.abi, dappTokenData.address)
+      this.setState({ dappToken })
+      let dappTokenBalance = await dappToken.methods.balanceOf(this.state.account).call()
+      this.setState({ dappTokenBalance: dappTokenBalance.toString() })
+    }
+    else {
+      window.alert('DappToken contract not deployed to detected network.')
+    }
+
+    const tokenFarmData = TokenFarm.networks[networkId]
+    if(tokenFarmData) {
+      const tokenFarm = new web3.eth.Contract(TokenFarm.abi, tokenFarmData.address)
+      this.setState({ tokenFarm })
+      let stakingBalance = await tokenFarm.methods.stakingBalance(this.state.account).call()
+      this.setState({ stakingBalance: stakingBalance.toString() })
+    }
+    else {
+      window.alert('TokenFarm contract not deployed to detected network.')
+    }
+
   }
 
   async loadWeb3() {
@@ -33,7 +72,14 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      account: '0x0'
+      account: '0x0',
+      daiToken: {},
+      dappToken: {},
+      tokenFarm: {},
+      daiTokenBalance: '0',
+      dappTokenBalance: '0',
+      stakingBalance: '0',
+      loading: true
     }
   }
 
