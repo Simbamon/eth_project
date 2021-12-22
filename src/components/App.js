af751e6ad6da70a4f40fcac7abd5092e2ac8733d
Simbamon <sbhan9511@gmail.com>
2021-12-22 19:06:18 +0900

    stake tokens

--- src/components/App.js
+++ src/components/App.js
@@ -71,6 +71,15 @@ class App extends Component {
     }
   }
 
+  stakeTokens = (amount) => {
+    this.setState({loading: true})
+    this.state.daiToken.methods.approve(this.state.tokenFarm._address, amount).send({from: this.state.account}).on('transactionHash', (hash) => {
+      this.state.tokenFarm.methods.stakeTokens(amount).send({from: this.state.account}).on('transactionHash', (hash) => {
+        this.setState({loading: false})
+      })
+    })
+  }
+
   constructor(props) {
     super(props)
     this.state = {
@@ -95,6 +104,7 @@ class App extends Component {
         daiTokenBalance = {this.state.daiTokenBalance}
         dappTokenBalance = {this.state.dappTokenBalance}
         stakingBalance = {this.state.stakingBalance}
+        stakeTokens={this.stakeTokens}
       />
     }
 
