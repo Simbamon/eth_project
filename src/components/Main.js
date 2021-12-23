8ee1582bd6bc429c285f0dcef70f5eb216f94bd1
Simbamon <sbhan9511@gmail.com>
2021-12-23 20:38:30 +0900

    Finished

--- src/components/Main.js
+++ src/components/Main.js
@@ -53,6 +53,15 @@ class Main extends Component {
               </div>
               <button type="submit" className="btn btn-primary btn-block btn-lg">STAKE</button>
             </form>
+            <button
+              type="submit"
+              className="btn btn-link btn-block btn-sm"
+              onClick={(event) => {
+                event.preventDefault()
+                this.props.unstakeTokens()
+              }}>
+                UNSTAKE
+            </button>
           </div>
         </div>
       </div>
